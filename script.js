c3bb68a0f1435c599c10a2f02b147cdadff9804a
DcSavage4L <dcsavage06@gmail.com>
2025-08-07 21:36:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Thanks for contacting DelishBite! We’ll get back to you shortly.');
+});
